01c6505854ab5556e7fa615b1ccef916b4d49707
decinemaat <info@decinemaat.nl>
2022-12-13 22:57:35 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+
+    // fontSize: {
+
+    // },
+    extend: {
+      colors: {
+        'zwart': '#1D1D1F',
+        'white' : '#FFFFFF',
+        'achtergrond' : '#cccccc',
+        'blauw1': '#4187AB',
+        'blauw2': '#214456',
+      },
+      fontFamily: {
+        'inter': ["'Inter'", 'sans-serif'],
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
